d42ebb22b0c6833f8a4eafd3db8214ae13c33dba
Haeata <hbal7849@uni.sydney.edu.au>
2021-04-07 18:20:40 +1000

    asd

--- vm.c
+++ vm.c
@@ -46,6 +46,7 @@ void init_registers(BYTE *registers) {
 int run(struct PMEM *pmem, BYTE *ram, BYTE *registers) {
 
 	while (registers[PC] < pmem->num_inst) {
+		printf("");
 
 
 		switch(pmem->inst[registers[PC]].opcode) {
